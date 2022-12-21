5d0fd4c4e7dc54e4738d9446578b925606e10112
afonsocosta <acoesdividendos@gmail.com>
2022-12-21 13:56:15 +0000

    first commit

---
+++ dashboard.py
@@ -0,0 +1,177 @@
+import streamlit as st
+import pandas as pd
+import requests
+from webull import webull
+import datetime
+import helper
+from streamlit_autorefresh import st_autorefresh
+
+
+influxCoockie = 'influxdb-oss-session=R_ywq0fgBLDkTTNr5e-iOFteReHuXIGBMxOUN9Yc4dScLCq4Fb8eTWPsj3VzYQl77ZRGLWu-pPXmSNaUUn7bQg==; PHPSESSID=s9kc4evui3hogtas2ngipni5n7; _pk_id.1.0e2b=ed6cf3ec48bfd1c5.1670520315.; grafana_session=c3bb9e6cf9bc370b35cd1a07abf8f762'
+helper.setPageConfig()
+wb = webull()
+col1, col2, col3, col4, col5, col6, col7, col8, col9= st.columns([4, 4, 4, 4, 4, 3, 3, 3, 3], gap='medium')
+col10, col11, col12, col13 = st.columns([3, 3, 3, 2])
+
+
+def getPricesForCanvas():
+    tickerArray = []
+    url = 'https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=SPY%7CQQQ%7CIWM%7CTLT%7C.VIX%7C@CL.1%7C@GC.1%7C@W.1%7C@S.1%7C@C.1%7CCADUSD=%7CJPYUSD=%7CGBP=%7CEUR=%7CAUD=%7CPLTR%7CAAPL%7CAMZN%7CGOOG%7CTSLA&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1'
+    if not helper.time_in_range():
+        url = 'https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=@SP.1%7C@ND.1%7C@TFS.1%7CTLT%7C.VIX%7C@CL.1%7C@GC.1%7C@W.1%7C@S.1%7C@C.1%7CCADUSD=%7CJPYUSD=%7CGBP=%7CEUR=%7CAUD=%7CPLTR%7CAAPL%7CAMZN%7CGOOG%7CTSLA&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1'
+    request = requests.get(url)
+    data = request.json()['FormattedQuoteResult']['FormattedQuote']
+    for ticker in data:
+        tickerFormatted = helper.getCNBCPrices(ticker)
+        tickerArray.append(tickerFormatted)
+    insertCanvasForTikers(tickerArray)
+
+
+def clockAndWeather():
+    fontSize = '27px'
+    date_time = datetime.datetime.now().strftime("%d-%m %H:%M:%S")
+    date,time = date_time.split()
+    queryWeather = 'SELECT%20mean(%22temperature%22)%20FROM%20%22weather%22%20WHERE%20time'
+    lastTemp = helper.getInlfuxQuery(queryWeather, influxCoockie)
+    queryUpTime = 'node_time_seconds%7Binstance%3D%22node_exporter%3A9100%22%2Cjob%3D%22node_exporter%22%7D+-+node_boot_time_seconds%7Binstance%3D%22node_exporter%3A9100%22%2Cjob%3D%22node_exporter%22%7D'
+    upTime = helper.getPrometheusQuery(queryUpTime, influxCoockie)
+    upTime = '{:0,.2f} days'.format(float(upTime / 3600 / 24))
+    runningContainersQuery = 'engine_daemon_container_states_containers%7Bstate%3D%7E%22running%22%7D'
+    runningContainers = helper.getPrometheusQuery(runningContainersQuery, influxCoockie)
+    stoppedContainersQuery = 'engine_daemon_container_states_containers%7Bstate%3D%7E%22stopped%22%7D'
+    stoppedContainers = helper.getPrometheusQuery(stoppedContainersQuery, influxCoockie)
+    color = helper.getTempColor(lastTemp)
+    with col9:
+        st.markdown(f"<p style='text-align: center; margin-top:40px; font-famlily: Arial Rounded MT Bold';><span style='font-size: {fontSize}; margin-top: 0;'>{time}</style></span><br><span style='text-align: center; font-size: {fontSize}; margin-top: 0;'>{date} | </style></span></style><span style='color: {color}; font-size: {fontSize}; margin-top: 0;'>{'{:0,.2f}°C'.format(lastTemp)}</style></span></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='text-align: center; font-famlily: Arial Rounded MT Bold;'>Up Time</style></span><br><span style='font-size: {fontSize}; margin-top: 0;'>{upTime}</style></span></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='text-align: center; font-famlily: Arial Rounded MT Bold;'><span style='font-size: 18px'>Stopped Contaienrs</style></span><br><span style='font-size: 36px; color: rgb(255, 62, 62)'>{int(stoppedContainers)}</span></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='text-align: center; font-famlily: Arial Rounded MT Bold; margin-top:-15px'><span style='font-size: 18px'>Running Contaienrs</style></span><br><span style='font-size: 36px; color: rgb(115, 191, 105)'>{int(runningContainers)}</span></p>", unsafe_allow_html=True)
+
+
+def insertGauges():
+    with col6:
+        queryCPUUsage = 'SELECT(%22usage_idle%22)%20FROM%20%22cpu%22%20WHERE%20(%22cpu%22%20%3D%20%27cpu-total%27)%20AND%20time'
+        cpuUsage = helper.getInlfuxQuery(queryCPUUsage, influxCoockie) * -1 + 100
+        queryCPUTemp = 'SELECT%20%22temp_input%22%20FROM%20%22sensors%22%20WHERE%20time'
+        cpuTemp = helper.getInlfuxQuery(queryCPUTemp, influxCoockie)
+        helper.rederGauge('CPU Usage','{:0,.2f}'.format(cpuUsage), '%', 0, 100, 10, 50, 80, 'cpu_usage')
+        helper.rederGauge('CPU Temp','{:0,.2f}'.format(cpuTemp), '°C', 0, 110, 10, 50, 80, 'cpu_temp')
+        dataf =  wb.get_bars('SPY', None,'d1', 120, 1)
+        helper.rederAreaChart(dataf, influxCoockie)
+        helper.getArraysForAreaChart('selly1_power', influxCoockie)
+    with col7:
+        queryRAMUsage = 'SELECT%20%22used_percent%22%20FROM%20%22mem%22%20WHERE%20time'
+        ramUsage = helper.getInlfuxQuery(queryRAMUsage, influxCoockie)
+        queryDisksage = 'SELECT%20%22used_percent%22%20FROM%20%22disk%22%20WHERE%20time'
+        diskUsage = helper.getInlfuxQuery(queryDisksage, influxCoockie)
+        helper.rederGauge('RAM Usage','{:0,.2f}'.format(ramUsage), '%', 0, 100, 10, 50, 80, 'cpu_usage1')
+        helper.rederGauge('Disk Used OS','{:0,.2f}'.format(diskUsage), '%', 0, 100, 10, 50, 80, 'cpu_usage4')
+    with col8:
+        sysLoadQuery = 'avg%28node_load15%7Binstance%3D%22node_exporter%3A9100%22%2Cjob%3D%22node_exporter%22%7D%29+%2F++count%28count%28node_cpu_seconds_total%7Binstance%3D%22node_exporter%3A9100%22%2Cjob%3D%22node_exporter%22%7D%29+by+%28cpu%29%29+*+100'
+        sysLoad = helper.getPrometheusQuery(sysLoadQuery, influxCoockie)
+        productionQuery = 'selly1_power'
+        production = helper.getPrometheusQuery(productionQuery, influxCoockie)
+        consumptionQuery = 'selly2_power'
+        consumption = helper.getPrometheusQuery(consumptionQuery, influxCoockie)
+        helper.rederGauge('Sys Load (15m avg)','{:0,.2f}'.format(sysLoad), '%', 0, 100, 10, 50, 80, 'cpu_usage3')
+        helper.createWattSpan(production, 'Production')
+        helper.createWattSpan(consumption, 'Consumption')
+
+def insertCanvasForTikers(tickerArray):
+    spyTicker = 'SPY'
+    qqqTicker = 'QQQ'
+    iwmTicker = 'IWM'
+    if not helper.time_in_range():
+        spyTicker = 'ES'
+        qqqTicker = 'NQ'
+        iwmTicker = 'RTY'
+
+    with col1:
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == spyTicker ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'EUR' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'CL' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'PLTR' ][0]), unsafe_allow_html=True)
+
+    with col2:
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == qqqTicker ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'GBP' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'GC' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'AAPL' ][0]), unsafe_allow_html=True)
+
+    with col3:
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == iwmTicker ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'CAD' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'ZW' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'AMZN' ][0]), unsafe_allow_html=True)
+
+    with col4:
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'TLT' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'AUD' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'ZC' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'GOOG' ][0]), unsafe_allow_html=True)
+
+    with col5:
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'VIX' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'JPY' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'ZS' ][0]), unsafe_allow_html=True)
+        st.markdown(helper.createCanvaForTicker([x for x in tickerArray if x["symbol"] == 'TSLA' ][0]), unsafe_allow_html=True)
+
+
+def insertCharts():
+    with col10:
+        dataf =  wb.get_bars('SPY', None,'d1', 120, 1)
+        helper.createChart(dataf, 'SPY')
+
+    with col11:
+        dataf =  wb.get_bars('VIX', None,'d1', 120, 1)
+        helper.createChart(dataf, 'VIX')
+
+    with col12:
+        columns = ['open_time','open', 'high', 'low', 'close', 'volume','close_time', 'qav','num_trades','taker_base_vol','taker_quote_vol', 'ignore']
+        binanceAPI = requests.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=170')
+        bitcoinData = binanceAPI.json()
+        bitcoindf = pd.DataFrame(bitcoinData, columns=columns, dtype=float)
+        bitcoindf.index = [pd.to_datetime(x, unit='ms').strftime('%Y-%m-%d %H:%M:%S') for x in bitcoindf.open_time]
+        helper.createChart(bitcoindf, 'BTC')
+
+
+def insertCryptoTable():
+    with col13:
+        returnArray = []
+        binanceAPI = requests.get('https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCUSDT","ETHUSDT","ADAUSDT","SOLUSDT","KDAUSDT","FLUXUSDT","BNBUSDT","NEARUSDT","AVAXUSDT"]')
+        criptoData = binanceAPI.json()
+        for crypto in criptoData:
+                    cryptoFormatted = {
+                        'Symbol': crypto['symbol'].replace('USDT', ''),
+                        'Price': '${:0,.2f}'.format(float(crypto['lastPrice'])),
+                        'Change': '{:0,.2f}%'.format(float(crypto['priceChangePercent']))
+                    }
+                    returnArray.append(cryptoFormatted)
+        reqKucoin = requests.get('https://api.kucoin.com/api/v1/market/stats?symbol=CRO-USDT')
+        returnArray.append({
+                        'Symbol': 'CRO',
+                        'Price': '${:0,.3f}'.format(float(reqKucoin.json()['data']['last'])),
+                        'Change': '{:0,.2f}%'.format(float(reqKucoin.json()['data']['changeRate']) * 100)
+                        })
+
+        dataframe = pd.DataFrame.from_records(returnArray)
+        helper.createTable(dataframe)
+
+
+if __name__ == '__main__':
+    #try:
+        clockAndWeather()
+        getPricesForCanvas()
+        insertCryptoTable()
+        insertCharts()
+        insertGauges()
+        st_autorefresh(interval=50000, limit=100000, key="fizzbuzzcounter")
+    #except:
+    #    print('error')
+
+        
+
+
+
+
+
---
+++ helper.py
@@ -0,0 +1,252 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+import datetime
+import pandas as pd
+import plotly.express as px
+import requests
+import plotly.graph_objects as go
+import datetime
+
+def setPageConfig():
+    st.set_page_config(
+   page_title="Dashboard",
+   page_icon="💰",
+   layout="wide",
+   initial_sidebar_state="collapsed",
+)
+
+    st.markdown(
+        """
+        <style>
+        footer {background-color: rgb(0,0,0); display: none !important}
+        [data-testid="stHeader"] { background-color: rgb(0,0,0); }
+        .block-container {
+            padding: 2rem 4rem 0rem;
+            background-color: rgb(0,0,0)
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def createCanvaForTicker(index):
+    ticker = index['symbol']
+    price = '${:0,.2f}'.format(index['price'])
+    if ticker == 'EUR' or ticker == 'AUD' or ticker == 'CAD' or ticker == 'GBP' or ticker == 'JPY':
+        price = '${:0,.4f}'.format(index['price'])
+    change = index['change']
+    wch_colour_box = (25, 74, 25)
+    if float(change.replace('%', '')) < 0:
+        wch_colour_box = (148, 25, 25)
+    wch_colour_font = (255, 255, 255)
+    change = '{:0,.2f}%'.format(float(change.replace('%', '')))
+
+    return f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                                {wch_colour_box[1]}, 
+                                                {wch_colour_box[2]}); 
+                            color: rgb({wch_colour_font[0]}, 
+                                    {wch_colour_font[1]}, 
+                                    {wch_colour_font[2]}); 
+                            font-family:Arial Rounded MT Bold;
+                            font-size: 36px;
+                            text-align: center;
+                            border-radius: 30px; 
+                            padding-top: 18px; 
+                            padding-bottom: 18px; 
+                            line-height:71px;'>
+                            {ticker}&nbsp;&nbsp;{change}
+                            </style><BR><span style='font-size: 35px; 
+                            margin-top: 0;'>{price}</style></span></p>"""
+                            
+
+def createTable(dataframe):
+    hide_table_row_index = """
+                        <style>
+                        table {margin-top: -2rem}
+                        thead tr th:first-child {display:none}
+                        thead tr {border-top: none !important}
+                        tbody th {display:none}
+                        tbody tr {border-top: none !important}
+                        </style>
+                        """
+
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    th_props = [('font-size', '25px'),
+    ('text-align', 'center'),
+    ('border', '0 !important'),
+    ('font-weight', 'bold'),
+    ('color', 'rgb(255,255,255)')]
+                                    
+    td_props = [('font-size', '25px'),
+    ('border', '0 !important'),
+    ('font-weight', 'bold'),
+    ('font-family', 'Arial Rounded MT Bold'), ('outline', 'none'), ('padding', '1px 25px')]
+                                            
+    styles = [dict(selector="th", props=th_props),
+    dict(selector="td", props=td_props)]
+
+    df2=dataframe.style.set_properties(**{'text-align': 'center'}).set_table_styles(styles).apply(upOrDown,axis=1)
+    st.write(df2.to_html(escape=False, index=False), unsafe_allow_html=True)
+
+def upOrDown(data, upColor='rgb(54,171,56)', downColor='rgb(200,14,14)'):
+    colorGreen = f'color: {upColor}'
+    colorRed = f'color: {downColor}'
+    change = float(data['Change'].replace('%',''))
+    colorToPaint = colorGreen if change > 0 else colorRed
+    return ['', '', colorToPaint]
+
+
+def getCNBCPrices(etf):
+    try:
+        if(etf['curmktstatus'] == 'POST_MKT' or etf['curmktstatus'] == 'PRE_MKT'):
+            change = etf['ExtendedMktQuote']['change_pct']
+            if change == "UNCH":
+                change = "0.00%"
+                print({'symbol': getTickerForName(etf['shortName']), 'price': float(etf['ExtendedMktQuote']['last'].replace(',','')), 'change': change})
+            return {'symbol': getTickerForName(etf['shortName']), 'price': float(etf['ExtendedMktQuote']['last'].replace(',','')), 'change': change}
+        else:
+            change = etf['change_pct']
+            if change == "UNCH":
+                change = "0.00%"
+            return {'symbol': getTickerForName(etf['shortName']), 'price': float(etf['last'].replace(',','')), 'change': change}
+    except:
+        return {'symbol': '', 'price': 0, 'change': float('0.00')}
+
+def getTickerForName(name):
+    if name == 'GOLD':
+        name = 'GC'
+    if name == 'OIL':
+        name = 'CL'
+    if name == 'WHEAT':
+        name = 'ZW'
+    if name == 'CORN':
+        name = 'ZC'
+    if name == 'SOYBEAN':
+        name = 'ZS'
+    if name == 'CADUSD':
+        name = 'CAD'
+    if name =='S&P FUT':
+        name = 'ES'
+    if name == 'NAS FUT':
+        name = 'NQ'
+    if name == 'RUS2K FUT':
+        name = 'RTY'
+    name = name.replace('/USD', '')
+    name = name.replace('USD', '')
+    return name
+    
+
+def rederGauge(text, value, format, min, max, split, lowerRange, higherRange, key):
+    color = 'rgb(255, 152, 48)'
+    if float(value) < lowerRange:
+        color = 'rgb(115, 191, 105)'
+    if float(value) > higherRange:
+        color = 'rgb(255, 62, 62)'
+    option = {
+    'series': [{
+      'type': 'gauge',
+      'center': ['50%', '70%'],
+      'startAngle': 180,
+      'endAngle': 0,
+      'min': min,
+      'max': max,
+      'splitNumber': split,
+      'itemStyle': {'color': color},
+      'progress': {'show': True,'width': 15},
+      'pointer': {'show': False},
+      'axisLine': {'lineStyle': {  'width': 15}},
+      'axisTick': {'distance': -25,'splitNumber': 5,'lineStyle': {  'width': 1,  'color': '#999'}},
+      'splitLine': {'distance': -32,'length': 14,'lineStyle': {  'width': 2,  'color': '#999'}},
+      'axisLabel': {'distance': -18,'color': '#999','fontSize': 12},
+      'anchor': {'show': False},
+      'title': {'show': False},
+      'detail': {'valueAnimation': True,'width': '100%','lineHeight': 15,'borderRadius': 8,'offsetCenter': [0, '-15%'],'fontSize': 24,'fontWeight': 'bolder','formatter': value + format,'color': color},
+      'data': [{'value': value}
+      ]}]
+    }
+
+    st_echarts(option, height="180px", width='210px', key=key, renderer='canva')
+    return st.markdown(f"<p style='text-align: center; width:210px; margin-top: -65px'><span style='text-align: center; font-size: 20px; margin-top: 0;'>{text}</style></span>", unsafe_allow_html=True)
+
+
+def getInlfuxQuery(query, cookie):
+    seconds_since_epoch = datetime.datetime.now().timestamp()
+    timeForQuery = round((seconds_since_epoch * 1000) - 18000 * 1000)
+    url = 'http://192.168.0.15:3000/api/datasources/proxy/8/query?db=influx&q=' + query + '%20%3E%3D%20' + str(timeForQuery) + 'ms&epoch=ms'
+    response = requests.get(url, headers={'Cookie': cookie}).json()
+    value = response['results'][0]['series'][0]['values'][len(response['results'][0]['series'][0]['values']) - 1][1]
+    if not value:
+        value = response['results'][0]['series'][0]['values'][len(response['results'][0]['series'][0]['values']) - 2][1]
+    return value
+
+
+def getPrometheusQuery(query, cookie):
+    endTime = datetime.datetime.now().timestamp()
+    startTime = endTime - 18000
+    url = 'http://192.168.0.15:9090/api/v1/query_range?query=' + query + '&start=' + str(startTime) + '&end=' + str(endTime) + '&step=14'
+    response = requests.get(url, headers={'Cookie': cookie}).json()
+    value = response['data']['result'][0]['values'] [len(response['data']['result'][0]['values']) - 1][1]
+    if not value:
+        value = response['data']['result'][0]['values'] [len(response['data']['result'][0]['values']) - 2][1]
+    return float(value)
+
+
+def createWattSpan(value, text):
+    color = 'rgb(115, 191, 105)'
+    if float(value) < 0:
+        color = 'rgb(115, 191, 105)'
+    if float(value) > 0:
+        color = 'rgb(255, 62, 62)'
+    value = '{:0,.0f} W'.format(float(value))
+    st.markdown(f"<p style='text-align: center; font-famlily: Arial Rounded MT Bold'><span style='font-size: 18px'>{text}</style></span><br><span style='font-size: 36px; color: {color}'>{value}</span></p>", unsafe_allow_html=True)
+    
+
+def rederAreaChart(dataf, cookie):
+    dfSheely1 = getArraysForAreaChart('selly1_power', cookie)
+    dfSheely2 = getArraysForAreaChart('selly2_power', cookie)
+    dfSheely1['Value2'] = dfSheely2['Value'].to_numpy()
+    dfSheely1.rename(columns = {'Value':'Production', 'Value2':'Consumption'}, inplace=True)
+    fig = go.Figure(layout=dict(width=877, height=330))
+    fig.add_trace(go.Scatter(x=dfSheely1['Time'], y=dfSheely1['Consumption'], fillcolor='rgba(255, 62, 62, 0.5)', fill='tozeroy', mode='none', name='Consumption', showlegend=False))
+    fig.add_trace(go.Scatter(x=dfSheely1['Time'], y=dfSheely1['Production'], fillcolor='rgb(115, 191, 105)', fill='tozeroy', mode='none', name='Production', showlegend=False))
+    fig.update_layout(margin=dict(l=10, r=10, t=0, b=0), paper_bgcolor="rgb(0,0,0)", title_font_family='Arial Rounded MT Bold', plot_bgcolor="rgb(0,0,0)", font_color="rgb(255,255,255)", xaxis=dict(showgrid=False, linecolor="rgba(255,255,255, 0.5)", showline=True, tickfont = dict(size=14)), yaxis=dict(showgrid=True, showline=True, linecolor="rgba(255,255,255, 0.5)", gridcolor="rgba(255,255,255, 0.2)", tickfont = dict(size=14)), font_family="Arial Rounded MT Bold", yaxis_title=None, xaxis_title=None, xaxis_tickformat = '%Hh %d-%b', title_font_size=25)
+    st.plotly_chart(fig)
+
+
+def createChart(dataf, title):
+    dataf.reset_index(inplace=True)
+    dataf = dataf.rename(columns = {'index':'timestamp'})
+    dataf = dataf.rename(columns={'timestamp': 'time'})
+    fig = px.line(dataf, x="time", y="close", title=title, width=640, height=449)
+    fig.update_layout(margin=dict(l=10, r=10, t=50, b=0), paper_bgcolor="rgb(0,0,0)", title_font_family='Arial Rounded MT Bold', plot_bgcolor="rgb(0,0,0)", font_color="rgb(255,255,255)", xaxis=dict(showgrid=False, linecolor="rgba(255,255,255, 0.5)", showline=True, tickfont = dict(size=14)), yaxis=dict(showgrid=True, showline=True, linecolor="rgba(255,255,255, 0.5)", gridcolor="rgba(255,255,255, 0.2)", tickfont = dict(size=14)), yaxis_tickformat = '$', font_family="Arial Rounded MT Bold", yaxis_title=None, xaxis_title=None, xaxis_tickformat = '%b-%Y', title_font_size=25, title={'text': title,'y':0.94,'x':0.5,'xanchor': 'center','yanchor': 'top'})
+    fig.data[0].line.color = "#d76c21"
+    fig.data[0].line.width = 2
+    st.plotly_chart(fig)
+
+
+def getArraysForAreaChart(query, cookie):
+    seconds_since_epoch = datetime.datetime.now().timestamp()
+    startTime = (seconds_since_epoch) - 660000
+    endTime = startTime + 660000
+    url = 'http://192.168.0.15:9090/api/v1/query_range?query=' + query + '&start=' + str(startTime) + '&end=' + str(endTime) + '&step=60'
+    response = requests.get(url, headers={'Cookie': cookie}).json()
+    valuesShelly1 = response['data']['result'][0]['values']
+    df = pd.DataFrame(data=valuesShelly1, columns=['Time', 'Value'])
+    df['Time'] = df['Time'].apply(lambda x :datetime.datetime.fromtimestamp(x))
+    df['Value'] = df['Value'].apply(lambda x :float(x))
+    return df
+
+def getTempColor(temp):
+    color = 'rgb(34 112 245)'
+    if temp > 7:
+        color = '#ffc624' 
+    if temp > 25:
+        color = 'rgb(255 95 36)'
+    return color
+
+
+def time_in_range():
+    start = datetime.time(14, 0, 0)
+    end = datetime.time(21, 0, 0)
+    current = datetime.datetime.now().time()
+    return start <= current <= end
